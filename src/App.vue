dd081e00f2471c30cb22bccf514635fde14cab2d
FrenzH <franciattom99@gmail.com>
2022-09-22 16:55:00 +0100

    fix

--- src/App.vue
+++ src/App.vue
@@ -30,11 +30,15 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+
+  
+}
+*{
   padding: 0;
   margin: 0;
   box-sizing: border-box;
   
   
-  
+
 }
 </style>
